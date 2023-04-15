9736dc33a6fbc11a4146e6c78ef15d5635915ef7
AMKir1 <ewsdcxzaq1Dgit>
2023-04-15 11:48:58 +0300

    Доработка отрисовки уровня

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -3,22 +3,72 @@ import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.Image
 import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.events.KeyboardEvent
+
+infix fun <A,B>A.x(b: B) = Pair(this,b)
 
 val sourceImage = Image()
+val level = Level(
+    floor = listOf(0..68, 71..85, 89..152, 155..212),
+
+    bushes = listOf(11 x 3, 23 x 1, 59 x 3, 89 x 2, 137 x 2, 71 x 1, 106 x 3, 118 x 1, 167 x 1),
+
+    clouds = listOf(
+        7 x 8 x 1, 19 x 9 x 1, 27 x 8 x 3, 36 x 9 x 2, 56 x 8 x 1, 67 x 9 x 1, 75 x 8 x 3,
+        87 x 9 x 1, 103 x 9 x 1, 123 x 8 x 3, 132 x 9 x 2, 152 x 8 x 1, 163 x 9 x 1,
+        171 x 8 x 3, 180 x 9 x 2, 200 x 8 x 1
+    ),
+
+    bricks = listOf( 20 x 3 x 5, 77 x 3 x 3, 80 x 7 x 8, 91 x 7 x 4, 94 x 3 x 1,
+        117 x 3 x 1, 100 x 3 x 2,  120 x 7 x 4,  128 x 7 x 4,  129 x 3 x 2,  168 x 3 x 4
+    ),
+
+    hills = listOf(0 x 2, 16 x 1, 48 x 2, 64 x 1, 96 x 2, 111 x 1, 144 x 2, 160 x 1, 192 x 2),
+
+    pipes = listOf(28 x 2, 38 x 3, 46 x 4, 163 x 2, 179 x 2),
+
+    pandoras = listOf(
+        16 x 3, 21 x 3, 22 x 7, 23 x 3, 78 x 3, 94 x 7, 105 x 3,
+        108 x 3, 108 x 7, 111 x 3, 129 x 7, 130 x 7, 170 x 3
+    ),
+    forwardSteps = listOf(134 x 4 x 0, 148 x 4 x 1, 181 x 8 x 1),
+
+    backwardSteps = listOf(140 x 4 x 0, 155 x 4 x 0),
+)
+
 lateinit var context: CanvasRenderingContext2D
+
 
 fun main() {
     window.onload = {
         val canvas = document.getElementById("canvas") as HTMLCanvasElement
         context = canvas.getContext("2d") as CanvasRenderingContext2D
         context.scale(3.0, 3.0)
-        context.fillStyle = "#7974ff"
-        context.fillRect(0.0, 0.0, canvas.width.toDouble(), canvas.height.toDouble())//768x720
+//        context.fillStyle = BACKGROUND_COLOR
+//        context.fillRect(0.0, 0.0, canvas.width.toDouble(), canvas.height.toDouble())//768x720
         sourceImage.src = TILES_IMAGE
-        val level = Level()
         sourceImage.onload = {
-            level.render()
+            document.addEventListener("keydown", { event ->
+                    val keyboardEvent = event as KeyboardEvent
+                    when (keyboardEvent.code) {
+                        "ArrowLeft" -> level.windowX -= 0.3
+                        "ArrowRight" -> level.windowX += 0.3
+
+                    }
+                    render()
+                })
         }
         Unit
     }
 }
+
+    const val CANVAS_WIDTH = 762.0
+    const val CANVAS_HEIGHT = 720.0
+    const val BACKGROUND_COLOR = "#7974FF"
+
+    fun render() {
+        context.clearRect(0.0, 0.0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        context.fillStyle = BACKGROUND_COLOR
+        context.fillRect(0.0, 0.0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        level.render()
+    }
--- src/main/kotlin/Level.kt
+++ src/main/kotlin/Level.kt
@@ -1,26 +1,64 @@
-class Level {
+typealias IntPair = Pair<Int, Int>
+typealias IntTriple = Pair<Pair<Int, Int>, Int>
+
+class Level(
+    val floor: List<IntRange> = emptyList(),
+    val bushes: List<Pair<Int, Int>> = emptyList(),
+    val hills: List<Pair<Int, Int>> = emptyList(),
+    val clouds: List<Pair<Pair<Int, Int>, Int>> = emptyList(),
+    val pipes: List<Pair<Int, Int>> = emptyList(),
+    val bricks: List<Pair<Pair<Int, Int>, Int>> = emptyList(),
+    val pandoras: List<Pair<Int, Int>> = emptyList(),
+    val forwardSteps: List<Pair<Pair<Int, Int>, Int>> = emptyList(),
+    val backwardSteps: List<Pair<Pair<Int, Int>, Int>> = emptyList(),
+) {
+
     var entities = setOf<Entity>()
     val CELL_SIZE = 16.0
     val FREE_AREA = 13
+    var windowX = 0.0
+
+    init {
+        floor.forEach { range ->
+            for (j in -1 downTo -2) {
+                for (i in range) {
+                    addFloor(i, j)
+                }
+            }
+        }
+
+        clouds.forEach { (indices, size) ->
+            val (i, j) = indices
+            addCloud(i, j, size)
+        }
+
+        bushes.forEach { (i, size) ->
+            addBush(i, size)
+        }
+
+        hills.forEach { (i, size) ->
+            addHill(i, size)
+        }
+    }
+
+
+
 
     fun render() {
-        addCloud(7,5,1)
-        addBush(11,3)
-        addHill(1, 3)
-        addFloor()
-
         for (entity in entities) {
-            drawSprite(entity.sprite, entity.x, entity.y)
+            if (entity.x + entity.sprite.w > windowX && entity.x < windowX + 16) {
+                drawSprite(entity.sprite, entity.x - windowX, entity.y)
+            }
         }
     }
 
     fun drawSprite(sprite: Sprite, x: Double, y: Double) {
         context.drawImage(
             sourceImage,
-            sx = sprite.si * CELL_SIZE + 1/3.0,
-            sy = sprite.sj * CELL_SIZE + 1/3.0,
-            sw = sprite.w * CELL_SIZE - 2/3.0,
-            sh = sprite.h * CELL_SIZE - 2/3.0,
+            sx = sprite.si * CELL_SIZE + 1 / 3.0,
+            sy = sprite.sj * CELL_SIZE + 1 / 3.0,
+            sw = sprite.w * CELL_SIZE - 2 / 3.0,
+            sh = sprite.h * CELL_SIZE - 2 / 3.0,
             dw = sprite.w * CELL_SIZE,
             dh = sprite.h * CELL_SIZE,
             dx = x * CELL_SIZE,
@@ -28,43 +66,42 @@ class Level {
         )
     }
 
-    fun addFloor() {
-        val FREE_AREA = 13
-        for (j in 16 downTo 15) {
-            for (i in 16 downTo 0) {
-               entities += Entity(i, FREE_AREA - j + floorSprite.h, floorSprite)
-            }
-        }
+    fun addFloor(i: Int, j: Int) {
+        entities += Entity(i = i, j = j, floorSprite)
     }
+
     fun addBush(i: Int, size: Int) {
-        entities += Entity(i = i, j = -2 + cloudSprite[0].h, bushSprites[0])
-        for (n in i + 1 .. size + i) {
-            entities += Entity(i = n, j = -2 + cloudSprite[0].h, bushSprites[1])
+        entities += Entity(i = i, j = -2, bushSprites[0])
+        for (n in i + 1..size + i) {
+            entities += Entity(i = n, j = -2, bushSprites[1])
         }
-        entities += Entity(i = size + i + 1, j = -2 + cloudSprite[0].h, bushSprites[2])
+        entities += Entity(i = size + i + 1, j = -2, bushSprites[2])
     }
+
     fun addCloud(i: Int, j: Int, size: Int) {
-        entities += Entity(i = i, j = j + cloudSprite[0].h, cloudSprite[0])
-        for (n in i + 1 .. size + i) {
-            entities += Entity(i = n, j = j + cloudSprite[0].h, cloudSprite[1])
+        entities += Entity(i = i, j = j, cloudSprite[0])
+        for (n in i + 1..size + i) {
+            entities += Entity(i = n, j = j, cloudSprite[1])
         }
-        entities += Entity(i = size + i + 1, j = j + cloudSprite[0].h, cloudSprite[2])
+        entities += Entity(i = size + i + 1, j = j, cloudSprite[2])
     }
+
     fun addHill(i: Int, height: Int) {
         var col = i + height - 3
-        for(j in 1 until height) {
+        for (j in 1 until height) {
             val size = (2 * j) - 1
             drawHillSection(col, height - j, size)
             col--
         }
         entities += Entity(i = height - 1, j = height - 1, hillSprites[1]) // top
     }
+
     fun drawHillSection(i: Int, j: Int, size: Int) {
-        entities += Entity(i = i, j = j - hillSprites[0].h, hillSprites[0]) // left side
-        for (n in i + 1 .. size + i) {
-            entities += Entity(i = n, j = j - hillSprites[0].h, hillSprites[4]) // middle
+        entities += Entity(i = i, j = j, hillSprites[0]) // left side
+        for (n in i + 1..size + i) {
+            entities += Entity(i = n, j = j, hillSprites[4]) // middle
         }
-        entities += Entity(i = size + i + 1, j = j - hillSprites[0].h, hillSprites[2]) // right side
+        entities += Entity(i = size + i + 1, j = j, hillSprites[2]) // right side
     }
 
 }
